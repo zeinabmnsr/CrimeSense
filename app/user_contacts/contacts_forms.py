5c2dd8cea52bee207bf9af608c8d9a97ba47dd7a
zeinab <zeinab_mansor1@hotmail.com>
2025-07-01 14:05:04 +0300

    fixing contacts_forms function

--- app/user_contacts/contacts_forms.py
+++ app/user_contacts/contacts_forms.py
@@ -1,17 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Regexp
+from wtforms.validators import DataRequired, ValidationError
+import re 
 
-# Lebanon: +961, 03/70/71/76/78/79...
-# UK: +44 or starting with 07
-lebanon_regex = r'^(\+961|0)?(3\d{6}|7[0-9]{7}|81\d{6})$'
-uk_regex = r'^(\+44\d{10}|07\d{9})$'
+
+def validate_phone_number(form, field):
+    phone = field.data.strip()
+    
+    # Regex: +961XXXXXXXX or 03XXXXXX or 07XXXXXX or 81XXXXXX
+    if not re.match(r"^(\+961\d{7,8}|03\d{6}|07\d{6}|81\d{6})$", phone):
+        raise ValidationError("Invalid phone number. Only Lebanese and UK formats are allowed.")
+
 
 class EmergencyContactForm(FlaskForm):
-    contact_name = StringField("Contact Name", validators=[DataRequired()])
-    phone_number = StringField("Phone Number", validators=[
-        DataRequired(),
-        Regexp(f"{lebanon_regex}|{uk_regex}",
-               message="Enter a valid Lebanese or UK phone number.")
-    ])
-    submit = SubmitField("Save")
+    name = StringField("Contact Name", validators=[DataRequired()])
+    phone_number = StringField("Phone Number", validators=[DataRequired(), validate_phone_number])
+    submit = SubmitField("Save Contact")
